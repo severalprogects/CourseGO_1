03d669a731ee5cd291fde7146152637fa6026868
sprogects <severalprogects@gamil.com>
2022-11-15 21:46:06 +0600

    add .gitignore and new module

--- les_4/1.3_importStruct/main.go
+++ les_4/1.3_importStruct/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/severalprogects/CourseGO_1/les_4/1.3_importStruct/student"
+)
+
+func main() {
+	myStud := student.NewStudent("Tom")
+	fmt.Println(myStud)
 }
