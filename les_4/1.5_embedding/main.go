beabd33ad5a45ec68cf93e03eda1b712597d18e9
sprogects <severalprogects@gamil.com>
2022-11-21 23:10:04 +0600

    add go.mod and go.sum in foulder 1.5

--- les_4/1.5_embedding/main.go
+++ les_4/1.5_embedding/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/severalprogects/CourseGO_1/les_4/1.5_embedding/student"
+)
+
+func main() {
+	Egor := student.NewStudent("Egor", 22, 1)
+	fmt.Println(Egor)
 }
